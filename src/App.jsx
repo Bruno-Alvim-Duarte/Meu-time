23c36e4563116b16b396a59ac3470e6485d571f2
Bruno Alvim Duarte <brunoalvimduarte@gmail.com>
2023-05-18 09:46:48 -0300

    criando a funcionalidade do login com a key, valida se a key é valida ou não

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/login" Component={ Login } />
+        <Route path="/login" element={ <Login /> } />
       </Routes>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 function Login() {
+  const [key, setKey] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    const myHeaders = new Headers();
+    // myHeaders.append('Content-Type', 'application/json');
+    // myHeaders.append('Accept', 'application/json');
+    // myHeaders.append('mode', 'no-cors');
+    myHeaders.append('x-rapidapi-key', key);
+    // myHeaders.append('Access-Control-Allow-Origin', '*');
+    // myHeaders.append(
+    //   'Access-Control-Allow-Headers',
+    //   'Access-Control-Allow-Headers, Content-Type, x-rapidapi-key, mode, Accept',
+    // );
+
+    const requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow',
+    };
+
+    fetch('https://v3.football.api-sports.io/status', requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.errors.token) {
+          return Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Chave inválida',
+          });
+        }
+        navigate('/home');
+      })
+      .catch((error) => console.log('error', error));
+  };
+
   return (
     <div>
       <h1>Login</h1>
       <label htmlFor="key-input">
         Key:
       </label>
-      <input type="text" name="key-input" id="key-input" />
-      <button>
+      <input
+        type="text"
+        name="key-input"
+        id="key-input"
+        onChange={ ({ target }) => setKey(target.value) }
+        value={ key }
+      />
+      <button onClick={ handleSubmit }>
         Entrar
       </button>
     </div>
